6519320695cf629c09ad7cad96302454600ce515
arunayesaswini <127669982+arunayesaswini@users.noreply.github.com>
2023-07-10 23:47:59 +0530

    Update EmployeeController.java

--- EmployeeMS/src/main/java/com/greatlearning/EmployeeMS/controller/EmployeeController.java
+++ EmployeeMS/src/main/java/com/greatlearning/EmployeeMS/controller/EmployeeController.java
@@ -23,21 +23,21 @@ public class EmployeeController {
 
 	@Autowired
 	EmployeeService employeeService;
-
+	// mapping to List of employees 
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 		List<Employee> listemployees = employeeService.getAllEmployees();
 		theModel.addAttribute("employees", listemployees);
 		return "employees/list-employees";
 	}
-
+	//mapping to create new employee details
 	@GetMapping("/new")
 	public String showFormForAdd(Model theModel) {
 		Employee e1 = new Employee();
 		theModel.addAttribute("employee", e1);
 		return "employees/employee-form";
 	}
-
+	//mapping to update employee details
 	@PostMapping("/edit")
 	public String edit(@RequestParam("employeeId") int eid, Model theModel) {
 		// getting the book from the service
@@ -50,13 +50,13 @@ public class EmployeeController {
 		return "employees/update-form";
 
 	}
-
+	// mapping to save the changes in employee details
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee semp) {
 		employeeService.addEmployee(semp);
 		return "redirect:/employee/list";
 	}
-
+	// Deleting the record of employee details using employee id
 	@PostMapping("/delete")
 	public String deleteEmployee(@RequestParam("employeeId") int eid) {
 		employeeService.deleteEmployee(eid);
